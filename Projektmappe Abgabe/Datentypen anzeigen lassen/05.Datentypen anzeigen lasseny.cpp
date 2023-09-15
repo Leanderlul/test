b24ce62a7d062ca1dd41a1032bca243b4caae9f2
Leander <145124101+Leanderlul@users.noreply.github.com>
2023-09-15 14:13:23 +0200

    Add files via upload

---
+++ Projektmappe Abgabe/Datentypen anzeigen lassen/05.Datentypen anzeigen lasseny.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <limits.h>
+#include <float.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cout << "Datentyp\tBits\tMax-Wert\tMin-Wert" << endl;
+
+	cout << setw(52) << setfill('-') << "" << endl;
+
+	cout << "INT\t\t" << sizeof(int) * 8 << "\t" << INT_MAX << "\t" << INT_MIN << endl;
+
+	cout << "SHORT\t\t" << sizeof(short) * 8 << "\t" << SHRT_MAX << "\t\t" << SHRT_MIN << endl;
+
+	cout << "LONG\t\t" << sizeof(long) * 8 << "\t" << LONG_MAX << "\t" << LONG_MIN << endl;
+
+	cout << "CHAR\t\t" << sizeof(char) * 8 << "\t" << CHAR_MAX << "\t\t" << CHAR_MIN << endl;
+
+	cout << "FLOAT\t\t" << sizeof(float) * 8 << "\t" << FLT_MAX << "\t" << FLT_MIN << endl;
+
+	cout << setw(52) << setfill('-') << "" << endl;
